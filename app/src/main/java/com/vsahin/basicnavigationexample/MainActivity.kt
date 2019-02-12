6291c090496942404f40fdec0debb0a70bad7b7e
Volkan Şahin <nihas.naklov@gmail.com>
2019-02-12 22:18:28 +0300

    Replace action bar with navigation controlled toolbar

--- app/src/main/java/com/vsahin/basicnavigationexample/MainActivity.kt
+++ app/src/main/java/com/vsahin/basicnavigationexample/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.vsahin.basicnavigationexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(findViewById(R.id.my_nav_host_fragment))
+        NavigationUI.setupWithNavController(findViewById<Toolbar>(R.id.toolbar), navController)
     }
 }
